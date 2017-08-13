4bf73e88e57bc80f49902fd231d2267f6ba827c3
Bilal Itani <bilalitani1@gmail.com>
2017-08-12 22:36:16 -0400

    Add lerp func

---
+++ Assets/UIFader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFader : MonoBehaviour {
+
+    private void Start()
+    {
+        
+    }
+
+    public IEnumerator Lerp(float start, float end, float lerpTime = 1)
+	{
+		float _timeStartedLerping = Time.time;
+		float timeSinceStarted = Time.time - _timeStartedLerping;
+		float percentageComplete = timeSinceStarted / lerpTime;
+
+		while (true)
+		{
+			timeSinceStarted = Time.time - _timeStartedLerping;
+			percentageComplete = timeSinceStarted / lerpTime;
+
+			float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+            if (percentageComplete >= 1) break;
+
+			yield return new WaitForFixedUpdate();
+		}
+	}
+}
